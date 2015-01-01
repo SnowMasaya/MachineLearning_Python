2bd6849c7ad8129a9a3f1fc42509f733ebd467f6
Masaya Ogushi <snow.akogi.pgel@gmail.com>
2015-01-01 20:59:28 +0900

    file write

--- NLP3/split_test.py
+++ NLP3/split_test.py
@@ -3,6 +3,7 @@
 import unittest
 from split_NLP3 import splitPython
 from wc_NLP1_copy import wcPython
+from split_write import writePython
 
 class TestSeaquenceFunction(unittest.TestCase):
     def testWc(self):
@@ -17,6 +18,10 @@ class TestSeaquenceFunction(unittest.TestCase):
         wc = wcPython(fileName)
         wc.file_Read()
         self.assertEqual(wc.filedata, split.split_data2)
+        write = writePython("KEN_cut.txt",split.split_data)
+        write.file_write_split()
+        write2 = writePython("KEN_cut2.txt",split.split_data2)
+        write2.file_write_split()
 
 if __name__ == '__main__':
     unittest.main()
